adc497b4b157fb1962ff3169440fc1915e4452a2
Duvan Rosero <duvanrosero1995@gmail.com>
2020-05-23 09:48:59 -0500

    creamos la lista de categorias

--- js/listar.js
+++ js/listar.js
@@ -1,6 +1,36 @@
 //console.log("hola mundo");
 
 
+
+document.querySelector('table').addEventListener('click', (event) => {
+
+    var etiqueta = event.target;
+
+    if (etiqueta.nodeName == 'A') {
+
+
+
+        var valor = etiqueta.getAttribute('data');
+        console.log(valor);
+        var categorias = JSON.parse(localStorage.getItem('categorias'));
+
+        var categoriasAux = [];
+
+
+        categorias.forEach((cat, index) => {
+            if (cat.descripcion != valor) {
+                categoriasAux.push(cat);
+            }
+        });
+
+        localStorage.setItem('categorias', JSON.stringify(categoriasAux));
+
+    }
+
+    listar();
+
+});
+
 function listar() {
     var categorias;
 
@@ -11,12 +41,19 @@ function listar() {
         categorias = JSON.parse(localStorage.getItem('categorias'));
     }
 
+    registro.innerHTML = "";
+
     categorias.forEach((cat, index) => {
         console.log(cat);
 
         var nuevaFila = document.createElement('tr');
-
-        nuevaFila.innerHTML = ``;
+        nuevaFila.innerHTML = `
+            <td> ${index + 1} </td>
+            <td> ${cat.codigo} </td>
+            <td> ${cat.descripcion} </td>
+            <td> <a href='javascript:void(0);' data='${cat.descripcion}' >Elinimar</a> </td>
+        `;
+        registro.appendChild(nuevaFila);
 
     });
 }
